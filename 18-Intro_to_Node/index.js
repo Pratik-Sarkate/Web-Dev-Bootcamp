ed7a4e4fa73f50348ccfaa983c9d4b66d6193c33
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2022-03-14 20:37:47 +0530

    Intro to Node completed

---
+++ 18-Intro_to_Node/index.js
@@ -0,0 +1,8 @@
+var superheroes = require("superheroes");
+var supervillains = require("supervillains")
+
+var superheroName = superheroes.random();
+console.log(superheroName);
+
+var supervillainName = supervillains.random();
+console.log(supervillainName);
